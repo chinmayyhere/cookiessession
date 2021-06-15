097bd01022ac0509de7864fd531fe6d896669091
chinmayyhere <85386887+chinmayyhere@users.noreply.github.com>
2021-06-15 22:25:11 +0530

    Add files via upload

---
+++ login.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Vaibhav
+ */
+public class login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String myval="";
+            Cookie mycook[]=request.getCookies();
+            if(mycook!=null)
+            {
+            for(int i=0;i<mycook.length;i++)
+            {
+                if(mycook[i].getName().toString().equals("uname"))
+                {
+                    myval=mycook[i].getValue();
+                }
+            }
+            }
+           out.println("<form action='logincheck' method='post'><table border='1'><tbody><tr><td>User Name</td><td><input type='text' name='userid' value='"+myval+"'  /></td></tr>");
+           out.println("<tr><td>Password</td><td><input type='text' name='pwd' /></td></tr><tr><td colspan=2><input type='submit' value='Login' /><tr><td>");
+           out.println("<input name='rem' type='checkbox' value='y'>Remember Me Next time</td><tr><input type='button' value='Register' /></td></tr></tbody></table></form>");
+           
+         }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
